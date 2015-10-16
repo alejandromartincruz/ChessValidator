c8db74a22a511cda29436d70ad1d674afa0c385a
alejandromartincruz <socorreitor@gmail.com>
2015-10-16 18:25:40 +0200

    Added create a piece object inside the array

--- ChessValidator.rb
+++ ChessValidator.rb
@@ -6,7 +6,6 @@ class Main
 
 	def initialize
 		@board = BoardTable.new
-		#@WP = Pawn.new
 		actions
 	end
 
@@ -14,14 +13,15 @@ class Main
 		set_positions
 		@board.show_board
 		#movement("a7")
-		#@board.show_board
+		@board.show_board
 	end
 
 	def set_positions
 		@board.board.each_with_index {|item, index|
 			@board.board[index].each_with_index { |innerItem, innerIndex|
 			if @board.board[index][innerIndex] != 0
-  				innerItem
+
+  				creating_pieces(innerItem, index, innerIndex)
   			end
   				}
 			}
@@ -29,30 +29,29 @@ class Main
 	end
 
 	def movement(position)
-		here_is = who_is_here?(position)
-		#@WP.delete_position(@board.board)
-		#@WP.moves
-		#@WP.set_position(@board.board)
+		object_pos = @board.coodinates_translator(position)
+		puts @board.board[object_pos[0]][object_pos[1]].moves
+		
+		#@board.board[here_is[0]][here_is[1]].delete_position(@board.board)
+		#@board.board[here_is[0]][here_is[1]].moves
+		#@board.board[here_is[0]][here_is[1]].set_position(@board.board)
 	end
 
 	def who_is_here?(position)
-		#binding.pry
+		
 		here = @board.coodinates_translator(position)
-		here_is = @board.board[here[0]][here[1]]
+		here_is = @board.board[here[0]][here[1]].type
 		return @here_is
 	end
 
+	def creating_pieces(piece, index, innerIndex)
+		mapped_pieces = {W: {P: Pawn, K: "King"},
+						 B: {P: Pawn, K: "King"}}
+		
+		@board.board[index][innerIndex] = mapped_pieces[piece[0].to_sym][piece[1].to_sym].new([index, innerIndex])
+	end
 
 end
-
-
-
-
-
-
-
-
-
 
 
 game = Main.new
